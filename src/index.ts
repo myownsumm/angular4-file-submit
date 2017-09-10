46f179a8e97f86b61a27509219e1c385b20a825b
alexander.poltoratskiy <myownsumm@gmail.com>
2017-09-10 15:28:52 +0300

    fixed correct build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
+
 import {FileSubmitDirective} from './file-submit.directive';
 
 export * from './file-submit.directive';
